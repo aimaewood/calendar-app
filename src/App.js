0d1523d201a6d545a87642f51cc07e4cfd56501b
aimaewood <aimaewood@gmail.com>
2022-02-11 01:10:58 +0000

    24 divs added to visualise hour-long time periods throughout the day

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const hourBlocks = []
+  for(let i=0; i<24; i++ ){hourBlocks.push(i)}
+  console.log(hourBlocks)
   return (
     <div className="App">
+
       <div className="header">
-        <h1>Friday 11th February 2022</h1>
-        <h3>Schedule:</h3>
-        <hr />
+         <h1>Friday 11th February 2022</h1>
+         <h3>Schedule:</h3>
+         <hr />
       </div>
+
+      <div className="calendar-container">
+          {/*put addNewEvent form here if time allows, and style calendar-container using display:flex
+        so that the form and calendar hour-blocks are displayed side by side*/}
+          {hourBlocks.map((hour, index) => {
+              return(
+                  <div key={index} className="hourBlock">{hour.toString() + ":00"}</div>
+              )
+          })}
+      </div>
+
     </div>
   );
 }
