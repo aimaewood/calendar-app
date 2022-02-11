2f730bcdc47029aba2d36c4c1fb32120f511e6a6
aimaewood <aimaewood@gmail.com>
2022-02-11 01:19:46 +0000

    removing an unecessary console.log

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-
 function App() {
 
   const hourBlocks = []
   for(let i=0; i<24; i++ ){hourBlocks.push(i)}
-  console.log(hourBlocks)
+  
   return (
-    <div className="App">
+    <div className="app">
 
       <div className="header">
          <h1>Friday 11th February 2022</h1>
@@ -19,7 +18,7 @@ function App() {
         so that the form and calendar hour-blocks are displayed side by side*/}
           {hourBlocks.map((hour, index) => {
               return(
-                  <div key={index} className="hourBlock">{hour.toString() + ":00"}</div>
+                  <div key={index} className="hour-block">{hour.toString() + ":00"}</div>
               )
           })}
       </div>
