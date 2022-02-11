03c997b8155e5654f01cafef90b360a546d65d04
aimaewood <aimaewood@gmail.com>
2022-02-11 00:40:28 +0000

    Schedule title featuring today's date added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>Friday 11th February 2022</h1>
+        <h3>Schedule:</h3>
+        <hr />
+      </div>
     </div>
   );
 }
