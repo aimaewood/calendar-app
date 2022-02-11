a14c5b050af5785e9ca0854dff402757e0fe9804
aimaewood <aimaewood@gmail.com>
2022-02-11 01:44:54 +0000

    added hardcoded events and written code to populate time-blocks with a div for each event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import {useState} from "react";
 import './App.css';
 function App() {
 
+  let [events, updateEvents] = useState([{title: "Aerial hoop class", start: 18, duration: 1.5}, {title: "Laka Interview", start: 14, duration: 1}, {title: "Yoga", start: 9, duration:2}])
   const hourBlocks = []
   for(let i=0; i<24; i++ ){hourBlocks.push(i)}
   
@@ -20,6 +22,13 @@ function App() {
               return(
                   <div key={index} className="hour-block">{hour.toString() + ":00"}</div>
               )
+          })}
+          {events.map((item, index) => {
+            return(
+                <div key={index} style={{height: item.duration*60 + "px", width: "40%", position: "absolute", top: item.start*60, left: "150px", backgroundColor: "Aquamarine", zValue: 5}}>
+                  <h3>{item.title}</h3>
+                </div>
+            )
           })}
       </div>
 
